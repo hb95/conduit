53b227ee1407fd6ea289376e348bae4190ea0e47
hb95 <horvathb95@gmail.com>
2023-04-14 02:09:11 +0200

    login and reg in progress

--- src/tests/system/conduit_page.py
+++ src/tests/system/conduit_page.py
@@ -3,12 +3,33 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver import Chrome
+import time
 
 
 class ConduitPage(GeneralPage):
     def __init__(self, browser: Chrome):
         super().__init__(browser, 'http://localhost:1667/')
 
+    def button_register(self):
+        return WebDriverWait(self.browser, 5).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//a[@class="nav-link"]')))[1]
+
     def button_login(self):
         return WebDriverWait(self.browser, 5).until(
-            EC.presence_of_element_located((By.XPATH, '//a[@class="nav-link"]')))
+            EC.presence_of_all_elements_located((By.XPATH, '//a[@class="nav-link"]')))[0]
+
+    def input_username(self):
+        return WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Username"]')))
+
+    def input_email(self):
+        return WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Email"]')))
+
+    def input_password(self):
+        return WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Password"]')))
+
+    def button_signin_signup(self):
+        return WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//button[@class="btn btn-lg btn-primary pull-xs-right"]')))
--- src/tests/system/test_conduit.py
+++ src/tests/system/test_conduit.py
@@ -1,20 +1,42 @@
 from conduit_page import ConduitPage
 from configuration import set_chrome_driver_local, set_chrome_driver_remote
 
-
 class TestConduit:
+
     def setup_method(self):
+        #self.page = ConduitPage(set_chrome_driver_local())
         self.page = ConduitPage(set_chrome_driver_remote())
         self.page.open()
         self.page.maximize()
 
     def teardown_method(self):
-        self.page.quit()
+        #self.page.quit()
+        pass
+
+    def test_registration_pos(self):
+        self.page.button_register().click()
+        self.page.input_username().send_keys('albert08')
+        self.page.input_email().send_keys('almodo.albert@almond.com')
+        self.page.input_password().send_keys('pontY738')
+        self.page.button_signin_signup().click()
+
+    def test_registration_neg(self):
+        self.page.button_register().click()
+        self.page.input_username().send_keys('benny')
+        self.page.input_email().send_keys('almodo.albert')
+        self.page.input_password().send_keys('pontY738')
+        self.page.button_signin_signup().click()
 
     def test_login_pos(self):
         self.page.button_login().click()
-        assert 7==7
+        self.page.input_email().send_keys('almodo.albert@almond.com')
+        self.page.input_password().send_keys('pontY738')
+        self.page.button_signin_signup().click()
+        assert 7 == 7
 
     def test_login_neg(self):
         self.page.button_login().click()
-        assert 7==7
+        self.page.input_email().send_keys('almodo.albert')
+        self.page.input_password().send_keys('pontY738')
+        self.page.button_signin_signup().click()
+        assert 7 == 7
